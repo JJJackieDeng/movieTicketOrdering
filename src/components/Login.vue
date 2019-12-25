c608574988b16a590ecc0fa119652adcea9a9b43
JJJackieDeng <15975403320@163.com>
2019-12-26 00:04:55 +0800

    修改登录同步请求 #3

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -51,9 +51,12 @@
             login(){
                 this.$refs.loginFormRef.validate(async valid => {
                     if (!valid)  return;
-                    const {data: res}=await this.$http.post('login',this.loginForm);
+                    const {data: res}=await this.$http.post("login",this.loginForm);
                     if(res.meta.status !==200) return this.$message.error("登录失败");
-                    this.$message.success("登录成功");
+                    this.$message.success("登录成功"); //2019年12月25日23:44:57 meta.status未设置正常，因此提示失败
+                    window.sessionStorage.setItem("token",res.meta.token);
+                    /*到主页面*/
+                    this.$router.push('/Main');
                 });
             }
         }
