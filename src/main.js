56dfe4142c77ee72aa3d6289f3ef63be90df86c3
JJJackieDeng <15975403320@163.com>
2019-12-26 10:16:56 +0800

    修改登陆细节 #1

--- src/main.js
+++ src/main.js
@@ -5,32 +5,30 @@ import router from './router'
 import store from './store'
 import './assets/css/global.css'
 import ElementUi from 'element-ui'
-import {Button} from "element-ui";
-import {Form,FormItem} from "element-ui";
+import {Form,FormItem,Message,Button} from 'element-ui';
 /*
 import {Input} from "element-ui";
 */
 //导入message弹窗
-import {Message} from "element-ui";
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
 //导入字体图标
 import '../src/assets/font/iconfont.css'
 
 import axios from 'axios'
 //配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'  //本地环境
-Vue.prototype.$http=axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';  //本地环境
+Vue.prototype.$http=axios;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(ElementUi)
-Vue.use(Button)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.prototype.$message= Message
+Vue.use(ElementUi);
+Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.prototype.$message= Message;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
+/*import Vue from 'vue'*/
 import VueRouter from 'vue-router'
 import Login from "@/components/Login";
 import Main from  "@/components/Main"
+import {Vue} from "vue/types/vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 /*
 export default new VueRouter({
@@ -39,14 +40,17 @@ const router = new VueRouter({
             component: Main
         }
     ]
-})
+});
 
 //挂载路由导航守卫
 router.beforeEach((to,from,next)=>{
     //to代表即将访问的路径，from表示从哪个路径跳转而来，next表示放行
     if(to.path === '/login') return next();
     //获取token
-    const tokenStr = window.sessionStorage.getItem('token')
-    if(!tokenStr) return next('/login') //用户没有登录,强制跳转到登录页面
+    const tokenStr = window.sessionStorage.getItem('token');
+    if(!tokenStr) return next('/login'); //用户没有登录,强制跳转到登录页面
     next()
-})
+});
+
+export default router
+
