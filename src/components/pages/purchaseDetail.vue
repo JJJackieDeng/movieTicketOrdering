72209133537903e152d6ceea70f4b53a27c42c54
JJJackieDeng <15975403320@163.com>
2020-03-23 00:02:35 +0800

    与后端交互进行选座#1

--- src/components/pages/purchaseDetail.vue
+++ src/components/pages/purchaseDetail.vue
@@ -28,28 +28,15 @@
                     </div>
                     <div class="screen-center">
                         银幕中央
-                        <div class="mid-line"></div>
+                        <!--                        <div class"mid-line"=></div>-->
                     </div>
                     <!--主要业务功能实现-->
-                    <div class="inner-seat-wrapper" ref="innerSeatWrapper">
-                        <div v-for="(row,index) in seatRow" :key='index'>
-                            <!--这里的v-if很重要0，如果没有则会导致报错，因为seatArray初始状态为空-->
-                            <!--                        v-if="seatArray.length>0"-->
-                            <div v-if="seatArray.length>0">
-                                <div v-for="(col,index) in seatCol"
-                                     class="seat"
-                                     :style="{width:seatSize+'px',height:seatSize+'px'}"
-                                     :key="index">
-                                    <div class="inner-seat"
-                                         @click="handleChooseSeat(row-1,col-1)"
-                                         v-if="seatArray[row-1][col-1]!==-1"
-                                         :class="seatArray[row-1][col-1]===2?'bought-seat':(seatArray[row-1][col-1]===1?'selected-seat':'unselected-seat')">
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
+                    <div class="seatChoose">
+                        <!--根据座位是否已售来判断是否可选；如果可选，根据是否已点击来显示座位状态-->
+                        <span v-for="(li,index) in 200"
+                              :class="[isSell[index]?'bg-sited':isClick[index]?'bg-beChosen':'bg-sit']" :key="index"
+                              @click="isSell[index]?cantSelect():clickSeats(index)">
+                            </span>
                     </div>
                 </div>
             </el-col>
@@ -57,13 +44,14 @@
                 <div class="side">
                     <div class="movieInfoBox">
                         <el-row :gutter="10">
-                            <el-col :span="6">
-                                <img src="../../assets/img/Luffy2.jpg" alt="加载失败" style="height: 158px;width: 115px;">
+                            <el-col :span="12">
+                                <img src="../../assets/img/Luffy2.jpg" alt="加载失败" class="posters">
                             </el-col>
-                            <el-col :span="18" offset="6">
+                            <el-col :span="12">
                                 <span>
-                                    电影名称
-                                </span>
+                                    海贼王：狂热行动
+                                </span><br>
+                                <span>时长：101分钟</span>
                             </el-col>
                         </el-row>
                         <span>影院：</span><br>
@@ -76,22 +64,8 @@
                         </span><br>
                         <span>总价：</span><br>
 
-                        <!--                        <div class="btn-buy" @click="buySeat">-->
-                        <!--                            选定座位-->
-                        <!--                        </div>-->
                         <div class="btn-wrapper">
-                            <!--智能选择-->
-                            <template v-for="(item,index) in smartChooseMaxNum">
-                                <div class="btn-buy smart" @click="smartChoose(index+1)" :key='index'>
-                                    推荐选座{{index+1}}人
-                                </div>
-                            </template>
-                            <br>
-                            <el-button type="danger" @click="buySeat">确认选座</el-button>
-                            <!--不需要重置选座，用户如果想修改选座就得自己手点-->
-                            <!--                            <el-button class="btn-buy" @click="resetSeat">-->
-                            <!--                                重置座位-->
-                            <!--                            </el-button>-->
+                            <el-button type="danger" @click="confirmSeats">确认选座</el-button>
                         </div>
                     </div>
                 </div>
@@ -101,227 +75,81 @@
 </template>
 
 <script>
+    import {getMoiveSeats, confirmSeats} from '../../api/commonUrls'
+
     export default {
         data() {
             return {
-                //影院座位的二维数组,-1为非座位，0为未购座位，1为已选座位(绿色),2为已购座位(红色)
-                seatArray: [],
-                //影院座位行数
-                seatRow: 10,
-                //影院座位列数
-                seatCol: 20,
-                //座位尺寸
-                seatSize: '',
-                //推荐选座最大数量
-                smartChooseMaxNum: 5
-
+                chooseSeats: [], //已选座位
+                isClick: [], //座位是否选中
+                isSell: [], //已售不可选
             }
 
         },
         methods: {
-            //向前后某个方向进行搜索的函数,参数是起始行，终止行,推荐座位个数
-            searchSeatByDirection: function (fromRow, toRow, num) {
-                /*
-                 * 推荐座位规则
-                 * (1)初始状态从座位行数的一半处的后一排的中间开始向左右分别搜索，取离中间最近的，如果满足条件，
-                 *    记录下该结果离座位中轴线的距离，后排搜索完成后取距离最小的那个结果座位最终结果，优先向后排进行搜索，
-                 *    后排都没有才往前排搜，前排逻辑同上
-                 *
-                 * (2)只考虑并排且连续的座位，不能不在一排或者一排中间有分隔
-                 *
-                 * */
-
-                /*
-                 * 保存当前方向搜索结果的数组,元素是对象,result是结果数组，offset代表与中轴线的偏移距离
-                 * {
-                 *   result:Array([x,y])
-                 *   offset:Number
-                 * }
-                 *
-                 */
-                let currentDirectionSearchResult = [];
-
-                let largeRow = fromRow > toRow ? fromRow : toRow,
-                    smallRow = fromRow > toRow ? toRow : fromRow;
-
-                for (let i = smallRow; i <= largeRow; i++) {
-                    //每一排的搜索,找出该排里中轴线最近的一组座位
-                    let tempRowResult = [],
-                        minDistanceToMidLine = Infinity;
-                    for (let j = 0; j <= this.seatCol - num; j++) {
-                        //如果有合法位置
-                        if (this.checkRowSeatContinusAndEmpty(i, j, j + num - 1)) {
-                            //计算该组位置距离中轴线的距离:该组位置的中间位置到中轴线的距离
-                            let resultMidPos = parseInt((j + num / 2), 10);
-                            let distance = Math.abs(parseInt(this.seatCol / 2) - resultMidPos);
-                            //如果距离较短则更新
-                            if (distance < minDistanceToMidLine) {
-                                minDistanceToMidLine = distance;
-                                //该行的最终结果
-                                tempRowResult = this.generateRowResult(i, j, j + num - 1)
-                            }
-                        }
+            //判断是否选中座位
+            choseSeats(idnex) {
+                if (this.chooseSeats.length !== 0) {
+                    var index2 = this.chooseSeats.indexOf(idnex);
+                    if (index2 === -1) {
+                        this.chooseSeats.push(idnex);
+                        this.$set(this.isClick, idnex, true);
+                    } else {
+                        this.$set(this.isClick, idnex, false);
+                        this.chooseSeats.splice(index2, 1)
                     }
-                    //保存该行的最终结果
-                    currentDirectionSearchResult.push({
-                        result: tempRowResult,
-                        offset: minDistanceToMidLine
-                    })
-                }
-
-                //处理后排的搜索结果:找到距离中轴线最短的一个
-                //注意这里的逻辑需要区分前后排，对于后排是从前往后，前排则是从后往前找
-                let isBackDir = fromRow < toRow;
-                let finalReuslt = [], minDistanceToMid = Infinity;
-                if (isBackDir) {
-                    //后排情况,从前往后
-                    currentDirectionSearchResult.forEach((item) => {
-                        if (item.offset < minDistanceToMid) {
-                            finalReuslt = item.result;
-                            minDistanceToMid = item.offset;
-                        }
-                    });
                 } else {
-                    //前排情况，从后往前找
-                    currentDirectionSearchResult.reverse().forEach((item) => {
-                        if (item.offset < minDistanceToMid) {
-                            finalReuslt = item.result;
-                            minDistanceToMid = item.offset;
-                        }
-                    })
+                    this.chooseSeats.push(idnex);
+                    this.$set(this.isClick, idnex, true);
                 }
-
-                //直接返回结果
-                return finalReuslt
             },
-            //推荐选座,参数是推荐座位数目
-            smartChoose: function (num) {
-                //找到影院座位水平垂直中间位置的后一排
-                let rowStart = parseInt((this.seatRow - 1) / 2, 10) + 1;
-                //先从中间排往后排搜索
-                let backResult = this.searchSeatByDirection(rowStart, this.seatRow - 1, num);
-                if (backResult.length > 0) {
-                    this.chooseSeat(backResult);
-                    return
-                }
-                //再从中间排往前排搜索
-                let forwardResult = this.searchSeatByDirection(rowStart - 1, 0, num);
-                if (forwardResult.length > 0) {
-                    this.chooseSeat(forwardResult);
-                    return
-                }
-                //提示用户无合法位置可选
-                alert('无合法位置可选!')
-
+            //选座
+            clickSeats(idnex) {
+                this.choseSeats(idnex)
+                console.log(this.chooseSeats)
             },
-            /*辅助函数，判断每一行座位从i列到j列是否全部空余且连续
- *
- */
-            checkRowSeatContinusAndEmpty: function (rowNum, startPos, endPos) {
-                let isValid = true;
-                for (let i = startPos; i <= endPos; i++) {
-                    if (this.seatArray[rowNum][i] !== 0) {
-                        isValid = false;
-                        break;
+            cantSelect() {
+                this.$message.warning("该座位已售！");
+                return false;
+            },
+            //确定选座
+            confirmSeats() {
+                confirmSeats({
+                    seats: this.chooseSeats, //数组
+                    scheduleId: this.$route.query.scheduleId
+                }, 'post').then(res => {
+                    return res.json()
+                }).then(res => {
+                    if (+res.code === 200) {
+                        this.$message.success("选座成功！");
+                    } else {
+                        this.$message.error("选座失败！");
                     }
-                }
-                return isValid
+                }).catch(err => {
+                    console.log(err);
+                    this.$message.error("选座接口出现错误！");
+                })
             },
-            //辅助函数：返回每一行的某个合理位置的座位数组
-            generateRowResult: function (row, startPos, endPos) {
-                let result = [];
-                for (let i = startPos; i <= endPos; i++) {
-                    result.push([row, i])
-                }
-                return result
-            },
-            //辅助函数:智能推荐的选座操作
-            chooseSeat: function (result) {
-                let oldArray = this.seatArray.slice();
-                for (let i = 0; i < result.length; i++) {
-                    //选定座位
-                    oldArray[result[i][0]][result[i][1]] = 1
-                }
-                this.seatArray = oldArray;
-            },
-            //选定且购买座位
-            buySeat: function () {
-                //遍历seatArray，将值为1的座位变为2
-                let oldArray = this.seatArray.slice();
-                for (let i = 0; i < this.seatRow; i++) {
-                    for (let j = 0; j < this.seatCol; j++) {
-                        if (oldArray[i][j] === 1) {
-                            oldArray[i][j] = 2
-                        }
+            //获取座位状态
+            getSeats() {
+                getMoiveSeats({scheduleId: this.$route.query.scheduleId}, 'get').then(res => {
+                    return res.json()
+                }).then(res => {
+                    for (let i of res) {
+                        this.$set(this.isSell, i.row, true);
                     }
-                }
-                this.seatArray = oldArray;
-            },
-            //重置座位
-            resetSeat: function () {
-                //将所有座位的值变为0
-                let oldArray = this.seatArray.slice();
-                for (let i = 0; i < this.seatRow; i++) {
-                    for (let j = 0; j < this.seatCol; j++) {
-                        if (oldArray[i][j] !== -1) {
-                            oldArray[i][j] = 0
-                        }
-                    }
-                }
-                this.seatArray = oldArray;
-            },
-            //处理座位选择逻辑
-            handleChooseSeat: function (row, col) {
-                let seatValue = this.seatArray[row][col];
-                let newArray = this.seatArray;
-                //如果是已购座位，直接返回
-                if (seatValue === 2) return
-                //如果是已选座位点击后变未选
-                if (seatValue === 1) {
-                    newArray[row][col] = 0
-                } else if (seatValue === 0) {
-                    newArray[row][col] = 1
-                }
-                //必须整体更新二维数组，Vue无法检测到数组某一项更新,必须slice复制一个数组才行
-                this.seatArray = newArray.slice();
 
+                }).catch(err => {
+                    console.log(err);
+                    this.$message.error('获取座位状态出错！');
+                })
             },
-            //初始座位数组
-            initSeatArray: function () {
-                let seatArray = Array(this.seatRow).fill(0).map(() => Array(this.seatCol).fill(0));
-                this.seatArray = seatArray;
-                this.seatSize = this.$refs.innerSeatWrapper
-                    ? parseInt(parseInt(window.getComputedStyle(this.$refs.innerSeatWrapper).width, 10) / this.seatCol, 10)
-                    : 0;
-                //初始化不是座位的地方
-                this.initNonSeatPlace();
-            },
-            //初始化不是座位的地方
-            initNonSeatPlace: function () {
-                for (let i = 0; i < 9; i++) {
-                    this.seatArray[i][0] = -1;
-                }
-                for (let i = 0; i < 8; i++) {
-                    this.seatArray[i][this.seatArray[0].length - 1] = -1;
-                    this.seatArray[i][this.seatArray[0].length - 2] = -1;
-                }
-                for (let i = 0; i < 9; i++) {
-                    this.seatArray[i][this.seatArray[0].length - 3] = -1;
-                }
-                for (let i = 0; i < this.seatArray[0].length; i++) {
-                    this.seatArray[2][i] = -1;
-                }
-
-            }
         },
 
 
         // 过滤器
         filters: {
-            // 选座信息过滤器
-            f3(data) {
-                return (data[0] + 1) + "排 | " + (data[1] + 1) + "座";
-            },
+
             // 日期过滤器
             time(data) {
                 let date = new Date(data);
@@ -339,9 +167,8 @@
                 return price.toFixed(2);
             }
         },
-        mounted: function () {
-            this.initSeatArray(10, 20)
-
+        mounted() {
+            this.getSeats();
         }
     }
 
@@ -385,6 +212,7 @@
         height: 20px;
         background-position: center center;
         background-size: cover;
+        list-style: none;
         background-image: url("../../assets/img/bg-unsit.png");
     }
 
@@ -633,5 +461,23 @@
         margin: 0 auto;
         width: 1200px;
         height: 100%;
+    }
+
+    .posters {
+        height: 158px;
+        width: 115px;
+    }
+
+    .seatChoose {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        padding: 60px 20px;
+    }
+
+    .seatChoose span {
+        width: 35px;
+        margin: 5px 5px;
+        height: 35px;
     }
 </style>
