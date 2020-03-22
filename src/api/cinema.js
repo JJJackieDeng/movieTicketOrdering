c78b74df04c656974a5f9e0f9368b0de790e3792
JJJackieDeng <15975403320@163.com>
2020-03-22 15:46:38 +0800

    增加影院管理#1

---
+++ src/api/cinema.js
@@ -0,0 +1,16 @@
+import * as QS from "qs";
+import axios from 'axios'
+
+/**get*/
+let header = {'Content-Type': 'application/json'};
+/*post*/
+let header2 = {'Content-Type': 'application/x-www-form-urlencoded'};
+/*获取所有订单信息*/
+
+// export const getALLOrders =params =>{return axios.get('${host}/api/order/selectAll',{params:params})};
+export function getAllCinema(body, methods) {
+    return fetch('/front/api/cinema/selectAll?limit=' + body.limit + '&offset=' + body.offset, {
+        method: methods,
+        headers: header
+    })
+}
