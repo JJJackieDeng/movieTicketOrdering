a67e434a9887d8cbf102dc5e61c37c52ec45bb1b
JJJackieDeng <15975403320@163.com>
2020-04-17 15:45:29 +0800

    修改细节

--- src/components/manage/dashboard.vue
+++ src/components/manage/dashboard.vue
@@ -1,71 +1,81 @@
 <template>
     <div class="page">
-        <el-header>
-            <div class="headerTitle">电影票务系统后台管理系统</div>
-            <div class="headerRight">
-                <el-dropdown>
+        <div class="">
+            <el-header>
+                <div class="headerTitle">电影票务系统后台管理系统</div>
+                <div class="headerRight">
+                    <el-dropdown>
                 <span>
                     <i class="el-icon-s-custom" style="font-size:20px; margin-right:10px"></i>
                     <!--todo 目前获取不了-->
                 {{this.userName}}
                 </span>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item icon="el-icon-user" split-button="true">
-                            <span @click="toLogin">注销</span>
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item icon="el-icon-user" split-button="true">
+                                <span @click="toLogin">注销</span>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+            </el-header>
+            <div style="height: 1000px">
+                <div class="leftSide  al-height-100">
+                    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+                        <el-radio-button :label="false">展开</el-radio-button>
+                        <el-radio-button :label="true">收起</el-radio-button>
+                    </el-radio-group>
+                    <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen"
+                             @close="handleClose"
+                             :collapse="isCollapse">
+                        <el-menu-item index="1" @click="toManageHome">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">系统首页</span>
+                        </el-menu-item>
+                        <el-menu-item index="2" @click="toUser">
+                            <i class="el-icon-user"></i>
+                            <span slot="title">用户管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="3" @click="toMovie">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">影片管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="4" @click="toOrders">
+                            <i class="el-icon-goods"></i>
+                            <span slot="title">订单管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="5" @click="toCinema">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">影院管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="6" @click="toScore">
+                            <i class="el-icon-star-off"></i>
+                            <span slot="title">评分管理</span>
+                        </el-menu-item>
+                        <el-menu-item index="7" @click="toSchedule">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">场次管理</span>
+                        </el-menu-item>
+                    </el-menu>
+                </div>
+                <div style="height: 1000px" class="al-overflow-scroll al-overflow-scroll-hide-bar">
+                    <router-view class=""></router-view>
+                </div>
             </div>
-
-        </el-header>
-        <div class="leftSide">
-        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-            <el-radio-button :label="false">展开</el-radio-button>
-            <el-radio-button :label="true">收起</el-radio-button>
-        </el-radio-group>
-        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-                 :collapse="isCollapse">
-            <el-menu-item index="1"  @click="toManageHome">
-                <i class="el-icon-menu"></i>
-                <span slot="title">系统首页</span>
-            </el-menu-item>
-            <el-menu-item index="2"  @click="toUser">
-                <i class="el-icon-menu"></i>
-                <span slot="title">用户管理</span>
-            </el-menu-item>
-            <el-menu-item index="3"  @click="toMovie">
-                <i class="el-icon-menu"></i>
-                <span slot="title">电影管理</span>
-            </el-menu-item>
-            <el-menu-item index="4" @click="toOrders">
-                <i class="el-icon-setting"></i>
-                <span slot="title">订单管理</span>
-            </el-menu-item>
-            <el-menu-item index="5" @click="toOrders">
-                <i class="el-icon-setting"></i>
-                <span slot="title">影院管理</span>
-            </el-menu-item>
-            <el-menu-item index="6" @click="toOrders">
-                <i class="el-icon-setting"></i>
-                <span slot="title">评分管理</span>
-            </el-menu-item>
-        </el-menu>
         </div>
-        <router-view></router-view>
     </div>
 </template>
 
 <script>
     export default {
         name: "dashboard",
-        data(){
-            return{
+        data() {
+            return {
                 isCollapse: false
             }
         },
         methods: {
-            toManageHome(){
-                this.routes.push('manageHome')
+            toManageHome() {
+                this.$router.push('manageHome');
             },
             toUser() {
                 this.$router.push('user');
@@ -73,16 +83,22 @@
             toMovie() {
                 this.$router.push('movie');
             },
-            toMovieInfo() {
-                this.$router.push('movieInfo');
+            toScore() {
+                this.$router.push('score');
             },
             toOrders() {
                 this.$router.push('orders');
             },
-            toLogin(){
-              this.$router.push({
-                  path: '/login'
-              })
+            toCinema() {
+                this.$router.push('cinema');
+            },
+            toSchedule() {
+                this.$router.push('schedule');
+            },
+            toLogin() {
+                this.$router.push({
+                    path: '/login'
+                })
             },
             handleOpen(key, keyPath) {
                 console.log(key, keyPath);
@@ -95,29 +111,32 @@
             this.$router.push('/dashboard/manageHome');
         },
         created() {
-                this.$emit('Header', false);
+            this.$emit('Header', false);
+            // this.$emit('Footer', false);
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .headerRight{
+    .headerRight {
         position: absolute;
         right: 20px;
         vertical-align: middle;
         align-items: center;
     }
+
     .page {
         height: 100%;
         width: 100%;
 
 
         .leftSide {
-            width: 250px;
+            /*width: 250px;*/
+            width: auto;
             height: 100%;
             background-size: contain;
             background-position: center center;
-            position: relative;
+            /*position: relative;*/
             float: left;
             background-color: #ffffff;
 
